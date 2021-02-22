35f5e37197019f673e095977912076d27a6b10aa
DanielJang <daniel.jang.0621@gmail.com>
2021-02-22 17:42:54 +0900

    Refactored redux-saga logic and deleted unnecessary files

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import counter from "./counter";
-import todos from "./todos";
 import github, { githubSaga } from "./github";
 import { all } from "redux-saga/effects";
 
-const rootReducer = combineReducers({ counter, todos, github });
+const rootReducer = combineReducers({ github });
 
 export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
